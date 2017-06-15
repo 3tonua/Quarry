89418dff309fec3577f6c3a6571109fddada370c
3tonua <3ton.ua@gmail.com>
2017-06-15 13:50:16 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -67,7 +67,7 @@ $(document).ready(function() {
   $('#modal_close, #overlay').click( function(){
     $('#modal_form')
       .animate({opacity: 0, top: '45%'}, 200,
-        function(){ // пoсле aнимaции
+        function(){
           $(this).css('display', 'none');
           $('#overlay').fadeOut(400);
         }
